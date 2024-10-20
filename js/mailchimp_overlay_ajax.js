60252f57ac7d4325efad7ba3c8a0f184aa4d862d
Frank Thoeny <14485-FrankThoeny@users.noreply.drupalcode.org>
2024-10-19 17:44:05 -0700

    added position and type settings to the jquery dialog object.

--- js/mailchimp_overlay_ajax.js
+++ js/mailchimp_overlay_ajax.js
@@ -4,25 +4,26 @@
 
   Drupal.behaviors.mailchimpOverlayAjax = {
     attach: function (context, settings) {
-      
+
       //if the cookie has_enews is not set, then show the drupal.dialog window
-      if (!Cookies.get("has_enews")) {
+      if (!Cookies.get('has_enews')) {
 
         //Dialog Box. See (http://api.jqueryui.com/dialog/) for all available options.
         let ajaxSettings = {
-          url: settings.mailchimp_overlay.path,
-          progress: { type: 'throbber' },
-          dialogType: 'modal',
+          dialogType: settings.mailchimp_overlay.type,
           dialog: {
-            width: settings.mailchimp_overlay.dialog_width,
-            dialogClass: "mailchimp-overlay-dialog",
+            dialogClass: 'mailchimp-overlay-dialog',
+            position: settings.mailchimp_overlay.position,
+            width: settings.mailchimp_overlay.width,
           },
           httpMethod: 'GET',
+          progress: { type: 'throbber' },
+          url: settings.mailchimp_overlay.path,
         };
 
         //execute the AJAX Dialog Box.
-        Drupal.ajax(ajaxSettings).execute(); 
-        
+        Drupal.ajax(ajaxSettings).execute();
+
         //then set the cookie, so next time the drupal.dialog won't be displaying again. (name, value, days)
         Cookies.set('has_enews', '1');
       }
